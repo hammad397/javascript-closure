0f1afbfb5ecea13af10f36806ac8f1f38646b0b2
hammad rafique <hammadrafique397@gmail.com>
2023-03-13 23:49:50 +0500

    closure

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const closure = counter => {
-    return count = () => {
-        if(counter < 1){
-            console.log('Hi, you called me!');
-            counter ++;
-        }else{
-            console.log('You, already called me!')
-        }
+const closure = () => {
+  let counter = 0;
+  return () => {
+    if (counter < 1) {
+      console.log("Hi, you called me!");
+      counter++;
+    } else {
+      console.log("You, already called me!");
     }
-}
-const counterClosure = closure(0)
-counterClosure();
-counterClosure();
+  };
+};
+
+const counterClosure = closure();
 counterClosure();
 counterClosure();
 counterClosure();
